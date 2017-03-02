a54cee873f5e28c925fa65a0bd9664a96bb62119
watsy0007 <watsy0007@gmail.com>
2017-03-02 17:13:13 +0800

    feat(docker): update config

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,5 @@ FROM ruby:2.4
 WORKDIR /app
 ADD . /app
 RUN bundler install
-EXPOSE 4567
-CMD ["ruby", "server.rb", "-p", "4567"]
+EXPOSE 3000
+CMD ["puma", "--config", "puma.rb"]
--- server.rb
+++ server.rb
@@ -1,5 +1,12 @@
 require 'sinatra'
 
-get '/' do
-  'Hello world!'
+configure {
+  set :server, :puma
+}
+class App < Sinatra::Base
+  get '/' do
+    'Hello world!'
+  end
+
+  run! if app_file == $0
 end
